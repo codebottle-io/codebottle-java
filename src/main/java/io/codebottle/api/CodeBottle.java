d25253bc32d140668ffb1765807ccef93315700d
Kaleidox <burdoto@outlook.com>
2019-10-13 23:49:53 +0200

    Added lazy loading methods to CodeBottle.java

--- src/main/java/io/codebottle/api/CodeBottle.java
+++ src/main/java/io/codebottle/api/CodeBottle.java
@@ -1,12 +1,15 @@
 package io.codebottle.api;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import io.codebottle.api.model.Category;
 import io.codebottle.api.model.Language;
 import io.codebottle.api.model.Snippet;
@@ -15,10 +18,11 @@ import io.codebottle.api.rest.Endpoint;
 import org.jetbrains.annotations.Nullable;
 
 public final class CodeBottle {
+    public final CompletionStage<Void> lazyLoading;
+
     private final Map<Integer, Language> languageCache = new ConcurrentHashMap<>();
     private final Map<Integer, Category> categoryCache = new ConcurrentHashMap<>();
     private final Map<Integer, Snippet> snippetCache = new ConcurrentHashMap<>();
-
     private final String token;
 
     public CodeBottle() {
@@ -27,6 +31,8 @@ public final class CodeBottle {
 
     public CodeBottle(@Nullable String token) {
         this.token = token;
+
+        this.lazyLoading = CompletableFuture.allOf(requestLanguages(), requestCategories());
     }
 
     public Optional<String> getToken() {
@@ -56,6 +62,27 @@ public final class CodeBottle {
                 });
     }
 
+    public CompletableFuture<Collection<Language>> requestLanguages() {
+        return new CodeBottleRequest<Collection<Language>>(this)
+                .to(Endpoint.LANGUAGES)
+                .makeGET()
+                .then(data -> {
+                    synchronized (languageCache) {
+                        return StreamSupport.stream(data.spliterator(), false)
+                                .map(node -> getLanguageByID(node.path("id").asInt())
+                                        .map(entity -> entity.update(node))
+                                        .orElseGet(() -> {
+                                            final Language language = new Language(this, node);
+
+                                            languageCache.compute(language.getID(), (k, v) -> language);
+
+                                            return language;
+                                        }))
+                                .collect(Collectors.toList());
+                    }
+                });
+    }
+
     public Optional<Category> getCategoryByID(int id) {
         if (id == -1) return Optional.empty();
 
@@ -79,6 +106,27 @@ public final class CodeBottle {
                 });
     }
 
+    public CompletableFuture<Collection<Category>> requestCategories() {
+        return new CodeBottleRequest<Collection<Category>>(this)
+                .to(Endpoint.CATEGORIES)
+                .makeGET()
+                .then(data -> {
+                    synchronized (categoryCache) {
+                        return StreamSupport.stream(data.spliterator(), false)
+                                .map(node -> getCategoryByID(node.path("id").asInt())
+                                        .map(entity -> entity.update(node))
+                                        .orElseGet(() -> {
+                                            final Category category = new Category(this, node);
+
+                                            categoryCache.compute(category.getID(), (k, v) -> category);
+
+                                            return category;
+                                        }))
+                                .collect(Collectors.toList());
+                    }
+                });
+    }
+
     public Optional<Snippet> getSnippetByID(int id) {
         if (id == -1) return Optional.empty();
 
@@ -98,6 +146,27 @@ public final class CodeBottle {
                                 .orElseGet(() -> snippetCache.compute(id,
                                         // existing values don't matter here, as the cache access failed before
                                         (k, v) -> new Snippet(this, data)));
+                    }
+                });
+    }
+
+    public CompletableFuture<Collection<Snippet>> requestSnippets() {
+        return new CodeBottleRequest<Collection<Snippet>>(this)
+                .to(Endpoint.SNIPPETS)
+                .makeGET()
+                .then(data -> {
+                    synchronized (snippetCache) {
+                        return StreamSupport.stream(data.spliterator(), false)
+                                .map(node -> getSnippetByID(node.path("id").asInt())
+                                        .map(entity -> entity.update(node))
+                                        .orElseGet(() -> {
+                                            final Snippet snippet = new Snippet(this, node);
+
+                                            snippetCache.compute(snippet.getID(), (k, v) -> snippet);
+
+                                            return snippet;
+                                        }))
+                                .collect(Collectors.toList());
                     }
                 });
     }
