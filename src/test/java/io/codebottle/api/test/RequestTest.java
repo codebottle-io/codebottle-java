f2d3944f97042e909d065ce1effc75324c2d6986
Kaleidox <burdoto@outlook.com>
2019-10-19 18:07:50 +0200

    Create RequestTest.java

---
+++ src/test/java/io/codebottle/api/test/RequestTest.java
@@ -0,0 +1,33 @@
+package io.codebottle.api.test;
+
+import io.codebottle.api.CodeBottle;
+import io.codebottle.api.model.Snippet;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertNotNull;
+
+public class RequestTest {
+    private CodeBottle codeBottle;
+
+    @Before
+    public void setUp() {
+        codeBottle = CodeBottle.builder()
+                .build()
+                .waitForLazyLoading();
+    }
+
+    @Test
+    public void testRequestSnippetByID() {
+        final Snippet snippet = codeBottle.requestSnippetByID("1b6c6604d6").join();
+
+        assertNotNull("Requested Snippet is null!", snippet);
+    }
+
+    @Test
+    public void testRequestRevisionByID() {
+        final Snippet.Revision revision = codeBottle.requestSnippetRevisionByID("1b6c6604d6", 1).join();
+
+        assertNotNull("Requested Revision is null!", revision);
+    }
+}
